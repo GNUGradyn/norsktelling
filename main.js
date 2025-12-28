24d9584257ae2d0e904d4d42572c8d3a1dcab959
Gradyn Wursten <me@gradyn.com>
2025-12-28 13:05:57 -0700

    new settings

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 "use strict";
 
 let num = 0;
-let eng = "zero";
-let no = "null";
+let eng = "";
+let no = "";
+let mode = "seq";
+let displayMode = "words";
+
 const normalize = (str) => str
     .toLowerCase()
     .trim()
@@ -16,9 +19,41 @@ const normalize = (str) => str
     .replaceAll("-", "")
     .replaceAll("–", "")
     .replaceAll("—", "")
+    .replaceAll(",", "")
     .replaceAll("og", ""); // Allow omitting og
 
-const updateState = () => document.getElementById("eng-num").innerText = eng;
+const getRandInRange = () => Math.floor(Math.random() * 1000000);
+const getModeButtons = () => document.getElementById("mode").getElementsByClassName("selector-button");
+const getDisplayModeButtons = () => document.getElementById("display-mode").getElementsByClassName("selector-button");
+
+const updateState = () => {
+    eng = n2words(num, {lang: "en"});
+    no = normalize(n2words(num, {lang: "nb"}));
+    switch (displayMode) {
+        case "words":
+            document.getElementById("eng-num").innerText = eng;
+            break;
+        case "digits":
+            document.getElementById("eng-num").innerText = num.toLocaleString('en-US');
+            break;
+    }
+
+    Array.from(getModeButtons()).forEach((el) => {
+        if (el.dataset.mode === mode) {
+            el.classList = "selector-button selected"
+        } else {
+            el.classList = "selector-button";
+        }
+    });
+
+    Array.from(getDisplayModeButtons()).forEach((el) => {
+        if (el.dataset.mode === displayMode) {
+            el.classList = "selector-button selected"
+        } else {
+            el.classList = "selector-button";
+        }
+    });
+}
 
 window.onload = () => {
     const noNum = document.getElementById("no-num");
@@ -29,10 +64,45 @@ window.onload = () => {
     noNum.onkeyup = () => {
         if (normalize(noNum.value) === no) {
             noNum.value = "";
-            num++;
-            eng = n2words(num, {lang: "en"});
-            no = normalize(n2words(num, {lang: "nb"}));
+            switch (mode) {
+                case "seq":
+                    num++;
+                    break;
+                case "pot":
+                    num = num * 10;
+                    break;
+                case "rand":
+                    num = getRandInRange();
+                    break;
+            }
             updateState();
         }
     }
+
+    Array.from(getModeButtons()).forEach((el) => {
+        el.addEventListener("click", () => {
+            mode = el.dataset.mode;
+            switch (mode) {
+                case "seq":
+                    num = 0;
+                    break;
+                case "pot":
+                    num = 1;
+                    break;
+                case "rand":
+                    num = getRandInRange();
+                    break;
+            }
+            updateState();
+        })
+
+        Array.from(getDisplayModeButtons()).forEach((el) => {
+            el.addEventListener("click", () => {
+                displayMode = el.dataset.mode;
+                updateState();
+            });
+        });
+
+        updateState();
+    });
 }
